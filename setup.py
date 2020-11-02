76fb65f32c92b571001b186e09364774268c05d4
Whyse Rabbit <rabbithole@wondrousland.com>
2020-11-02 13:36:47 -0700

    setup.py edited for version update.

--- setup.py
+++ setup.py
@@ -13,9 +13,9 @@ with open("README.md", "r") as fh:
     LONG_DESCRIPTION = fh.read()
 
 setuptools.setup(
-    name=<PROJ_NAME>,
-    version="0.0.1",
-    author=<USERNAME>,
+    name=lambdata_whyse,
+    version="0.0.3",
+    author=WhyseRabbit,
     description="A collection of Data Science helper functions",
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
