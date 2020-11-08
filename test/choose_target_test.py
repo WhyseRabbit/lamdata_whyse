35271015fb0fd50c2ac1a0af9b2e7898172e3cd1
WhyseRabbit <whyse@centaur.com>
2020-11-08 15:14:27 -0700

    update testing process

--- test/choose_target_test.py
+++ test/choose_target_test.py
@@ -1,3 +1,12 @@
 import unittest
+from .lambdata_whyse import choose_target
 
-"""This is a temporary placeholder as I watch a previous lecture."""
+df = {
+    "yes" : [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
+    "no" : [1.45, 2.87, 3.99, 4.37, 5.66, 6.51]
+}
+
+class TestChoose_Target(unittest.TestCase):
+
+    def test_ct_function(self):
+
